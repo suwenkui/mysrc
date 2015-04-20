06ee80aced9bbe96905a783315b0baebc0a9fc1d
suwenkui <suwenkui@gmail.com>
2015-04-20 14:26:14 +0800

    add to branch dev

--- attribute.py
+++ attribute.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     dir(a)
     print 'object a is:',a
     print 'dir a',dir(a)
-    print 'dir A',dir(A)
+    #print 'dir A',dir(A)
     '''
     object a is: <__main__.A object at 0x7fa2ff189fd0>
 dir a ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
