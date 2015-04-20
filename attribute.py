56680dbe7afef3e0d02821d485ee4314755a953a
suwenkui <suwenkui@gmail.com>
2015-04-20 14:19:51 +0800

    add doc

--- attribute.py
+++ attribute.py
@@ -9,4 +9,11 @@ if __name__ == '__main__':
     a = A()
 
     dir(a)
-    print a,dir(a)
+    print 'object a is:',a
+    print 'dir a',dir(a)
+    print 'dir A',dir(A)
+    '''
+    object a is: <__main__.A object at 0x7fa2ff189fd0>
+dir a ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
+dir A ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
+    '''
