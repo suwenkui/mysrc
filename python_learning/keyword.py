6aecb356a71ebd4c5c85079fe5e224103e293e49
suwenkui <suwenkui@gmail.com>
2015-05-21 12:09:33 +0800

    add ua_parser.py

--- python_learning/keyword.py
+++ python_learning/keyword.py
@@ -1,5 +1,13 @@
 import  jieba
 __author__ = 'suwk'
 
+FILE_PATH = '../data/keywords_user.log'
+
+def split_data(path=FILE_PATH):
+    with open(path) as data:
+        for line in data:
+            print line
+            break
+
 if __name__ == '__main__':
-    pass
+    split_data()
